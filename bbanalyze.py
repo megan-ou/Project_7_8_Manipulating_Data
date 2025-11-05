bb12ef950ba6afe614720d4e4632fb2eccb3e899
meganou <ou27@up.edu>
2025-11-04 20:33:29 -0800

    MO - Setting up all dictionaries? I think I got them all?

--- bbanalyze.py
+++ bbanalyze.py
@@ -17,3 +17,20 @@ def bbanalyze(filename = "baseball.csv"):
         records
     """
     bbdat = pd.read_csv(filename)
+
+    #Construct empty dictionaries with null values to be populated later; basically initializing
+    # all values to keep track of the dictionaries within dictionaries. This is for my own sanity;
+    # I am unsure if it would be better to create them later on.
+    bbstats = dict.fromkeys(["record.count", "complete.cases", "player.count", "team.count",
+                             "league.count", "bb", "nl", "al", "records"])
+    bbstats["nl"] = dict.fromkeys(["dat", "players", "teams"])
+    bbstats["al"] = dict.fromkeys(["dat", "players", "teams"])
+    bbstats["records"] = dict.fromkeys(["odp", "pab", "hr", "hrp", "h", "hp", "sb", "sbp", "so",
+                                        "sop", "sopa", "bb", "bbp", "g"])
+    #I didn't want to use a for loop and instead use a list comprehension, but I was not sure
+    # how to structure it as bbstats["records"][key] = [list comprehension]
+    for i in bbstats["records"].keys:
+        bbstats["records"][i] = dict.fromkeys(["id", "value"])
+
+
+
