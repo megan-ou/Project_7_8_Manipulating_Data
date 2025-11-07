fe480e0fd868f436ee49da24efdbaf704b17d9f2
meganou <ou27@up.edu>
2025-11-06 19:03:35 -0800

    MO - Added execution tests and wrote helper method to get data subset

--- bbanalyze.py
+++ bbanalyze.py
@@ -1,3 +1,4 @@
+import math
 import pandas as pd
 
 def bbanalyze(filename = "baseball.csv"):
@@ -32,5 +33,21 @@ def bbanalyze(filename = "baseball.csv"):
     for i in bbstats["records"].keys:
         bbstats["records"][i] = dict.fromkeys(["id", "value"])
 
+    bbstats["al"]["dat"] = get_dat_subset(bbdat,"lg", "AL")
 
+def get_dat_subset(df, col, value):
+    """
+    Helper method that takes a subset of data based on a specific ID value (ex: taking a
+    subset of all National League Baseball players)
+    Args:
+        df (Pandas DataFrame): DataFrame that we are taking the subset out of
+        col (string): column name of the data subset we are extracting
+        value (string): what specific value from the subset that we are looking for
+
+    Returns: subset of database based on the id and value
+    """
+    if not isinstance(id, str) or not isinstance(value, str) or not isinstance(df, pd.DataFrame):
+        return math.nan
+
+    return df.query(f'{col} == "{value}"')
 
